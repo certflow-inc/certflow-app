f076acca8796f34413761cac9996bc091a29a135
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-03-19 09:20:19 -0300

    refactor: 💡 signup service adjustments

--- src/service/signup/signup.service.ts
+++ src/service/signup/signup.service.ts
@@ -4,32 +4,23 @@ import { ApiError, ApiResponse } from '@/types/api';
 import { Register } from '@/types/register';
 import { StatusCodes } from 'http-status-codes';
 
+/**
+ * Sends a sign-up request to the API with the provided registration data.
+ *
+ * @param {Register} register - The registration data to be sent in the request.
+ * @returns {Promise<ApiResponse<void>>} A promise that resolves to an ApiResponse
+ * indicating the success or failure of the sign-up operation.
+ *
+ * @throws Will throw an error if the response status is NOT_FOUND or INTERNAL_SERVER_ERROR,
+ * or if any other error occurs during the request.
+ */
+
 export async function signup(register: Register): Promise<ApiResponse<void>> {
   try {
-    const payload = {
-      type: register.type,
-      taxId: register.taxId.replaceAll('.', '').replaceAll('-', ''),
-      name: register.name,
-      mobilePhone: `+55${register.mobilePhone.replace(/\D/g, '')}`,
-      email: register.email,
-      password: register.password,
-      confirmPassword: register.confirmPassword,
-      ...(register.companyTaxId
-        ? {
-            companyTaxId: register.companyTaxId
-              .replaceAll('.', '')
-              .replaceAll('-', '')
-          }
-        : {}),
-      ...(register.companyName ? { companyName: register.companyName } : {})
-    };
-
     const response = await fetch(`${process.env.API_URL}/sign-up`, {
       method: 'POST',
-      body: JSON.stringify(payload),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      body: buildPayload(register),
+      headers: buildHeaders()
     });
 
     if (
@@ -56,4 +47,50 @@ export async function signup(register: Register): Promise<ApiResponse<void>> {
   } catch (_error: unknown) {
     throw new Error('Internal server error');
   }
+}
+
+/**
+ * Build the payload for the sign-up request.
+ * @param {Register} register The register data.
+ * @returns {string} The payload as a JSON string.
+ */
+function buildPayload(register: Register) {
+  const {
+    type,
+    taxId,
+    name,
+    mobilePhone,
+    email,
+    password,
+    confirmPassword,
+    companyName,
+    companyTaxId
+  } = register;
+
+  return JSON.stringify({
+    type,
+    taxId: taxId.replaceAll('.', '').replaceAll('-', ''),
+    name,
+    mobilePhone: `+55${mobilePhone.replace(/\D/g, '')}`,
+    email,
+    password,
+    confirmPassword,
+    ...(companyTaxId
+      ? {
+          companyTaxId: companyTaxId.replaceAll('.', '').replaceAll('-', '')
+        }
+      : {}),
+    ...(companyName ? { companyName: companyName } : {})
+  });
+}
+
+/**
+ * Build headers for the request.
+ *
+ * @returns {Record<string, string>} headers object
+ */
+function buildHeaders() {
+  return {
+    'Content-Type': 'application/json'
+  };
 }
