706ea66cfca68d858112a5d27bd7044824d7d80d
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-03-21 17:20:05 -0300

    feat: 🎸 logout button improvements

--- src/components/logout-button/logout-button.tsx
+++ src/components/logout-button/logout-button.tsx
@@ -2,19 +2,31 @@
 
 import { destroySession } from '@/lib/session';
 import { ROUTES } from '@/routes';
+import { LoaderCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Button } from '../ui/button';
 
 export function LogoutButton() {
   const router = useRouter();
+  const [isProcessing, setIsProcessing] = useState(false);
+
   return (
     <Button
       onClick={() => {
-        destroySession();
-        router.push(ROUTES.SIGNIN);
+        setIsProcessing(true);
+        destroySession()
+          .then(() => {
+            router.push(ROUTES.SIGNIN);
+          })
+          .finally(() => {
+            setIsProcessing(false);
+          });
       }}
+      disabled={isProcessing}
     >
-      Logout
+      Sair
+      {isProcessing && <LoaderCircle className="ml-2 h-4 w-4 animate-spin" />}
     </Button>
   );
 }
