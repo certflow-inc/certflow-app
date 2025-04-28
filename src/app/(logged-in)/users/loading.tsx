6f83eba1e759d1831346764c4bf91365b4a44302
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-04-28 11:56:44 -0300

    refactor: 💡 user loading page adjustments

--- src/app/(logged-in)/users/loading.tsx
+++ src/app/(logged-in)/users/loading.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function UserListLoagingPage() {
   return (
     <LoggedIn.Container>
-      <div className="flex h-full flex-col gap-6 p-2 min-[1130px]:h-1/3">
+      <div className="flex h-full flex-col gap-6 p-2">
         <div className="flex items-center justify-between gap-2">
           <Skeleton className="h-12 w-2/3" />
           <Skeleton className="h-12 w-50" />
